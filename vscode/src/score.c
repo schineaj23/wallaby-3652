3326f02fc89c93cfc0c633abeb6383775dd08f5b
Andrew Schineller <SchineAJ23@pwcs-edu.org>
2018-02-23 08:56:20 -0500

    cleaned up score() added comments

--- vscode/src/score.c
+++ vscode/src/score.c
@@ -17,30 +17,38 @@ bool readyScore()
 
 void score()
 {
+
+    //While it isn't ready to score, wait 1Msec
     while(!readyScore())
         msleep(1);
 
     closeClaw();
 
+    //Turn towards scoring zone
     spinLeft(100, 1000);
 
-    while(!isTracking())
-        moveForward(100, 1);
+    //Skip line into the zone
+    skipLine(FORWARD, 1);
 
+    //move forward into the zone
     moveForward(100, 500);
 
     openClaw();
 
-    while(!isTracking())
-        moveBackward(100, 1);
+    //skip line out of zone
+    skipLine(BACKWARD, 1);
 
+    //turn towards line
     spinRight(100, 1000);
 
+    //move towards line until starts tracking
     while(!isTracking())
         moveForward(100, 1);
     
+    //go back to executing trackLine() function
     trackLine();
     
+    //clear motor counters
    	cmpc(LEFT_MOTOR);
     cmpc(RIGHT_MOTOR);
 }
