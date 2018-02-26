782766ecfd9cfc1f6bebcfa687359ed90a221548
Andrew Schineller <SchineAJ23@pwcs-edu.org>
2018-02-26 14:17:50 -0500

    bool who??

--- vscode/include/util.h
+++ vscode/include/util.h
@@ -16,3 +16,5 @@ void correctPos();
 void skipLine(int direction, int linesToSkip);
 
 void getToLine();
+
+void trackEdge();
--- vscode/src/util.c
+++ vscode/src/util.c
@@ -1,7 +1,7 @@
+#include <stdbool.h>
 #include "util.h"
 #include "chassis.h"
 #include "score.h"
-#include <stdbool.h>
 
 #define NOT_TRACKING() analog(LINE_TRACKER) < 800
 #define TRACKING() analog(LINE_TRACKER) > 800
@@ -67,7 +67,6 @@ void correctPos()
 
 void trackLine()
 {
-
     correctPos();
 
     cmpc(RIGHT_MOTOR);
