6ffbb405689e9af347eb5a7366062c5ec004650b
Andrew Schineller <SchineAJ23@pwcs-edu.org>
2018-04-02 16:08:44 -0400

    updated build.sh

--- .vscode/build.sh
+++ .vscode/build.sh
@@ -2,8 +2,10 @@ echo @off
 echo trying to work
 ssh root@192.168.125.1
 echo Connecting to wallaby...
-cd ..
-cd /home\ Default User
+cd parksiderepo/somas
 echo Connected to Wallaby, building...
-make -f cpp
+make
+cd bin
 echo Running program...
+./botball_user_program
+exit
