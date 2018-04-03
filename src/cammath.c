df5abdefbf422a0d833f2a9be3f664f616e5073f
Andrew Schineller <SchineAJ23@pwcs-edu.org>
2018-04-03 16:58:10 -0400

    working on filters and other algs for camera

--- src/cammath.c
+++ src/cammath.c
@@ -1,5 +1,7 @@
 #include "cammath.h"
 #include "draw.h"
+#include <kipr/camera.h>
+#include <kipr/geom.h>
 
 // Defaults for crates.
 const double OBJECT_HEIGHT_MM = 101.6;
@@ -22,4 +24,23 @@ bool isTrackable(int channel, int object)
 {
     if(get_object_confidence(channel, object) >= 0.5) return true;
     return false;
+}
+
+
+short idk_the_name(int color)
+{
+    int h = get_camera_height();
+    int w = get_camera_width();
+    char *image = get_camera_frame();
+    short[][] pos;
+    int x, y;
+    for(x=0;x<h;x++)
+        for(y=0;y<w;y++)
+        {
+            point2 p2;
+            p2.x = x; p2.y = y;
+            pixel p = get_camera_pixel(p2);
+            int color = p2.g;
+            pos[x][y] = &image + sizeof(image) + (h*x + y);
+        }
 }
