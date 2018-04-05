557275d40e77cdabe537e4a8c9134380f91f461a
Kate Schineller <schineaj23@pwcs-edu.org>
2018-04-05 13:29:49 -0500

    Finished Findpixel

--- src/cammath.c
+++ src/cammath.c
@@ -1,3 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////////////
+//
+//     _____           _        _     _        _____       _           _   _          
+//    |  __ \         | |      (_)   | |      |  __ \     | |         | | (_)         
+//    | |__) |_ _ _ __| | _____ _  __| | ___  | |__) |___ | |__   ___ | |_ _  ___ ___ 
+//    |  ___/ _` | '__| |/ / __| |/ _` |/ _ \ |  _  // _ \| '_ \ / _ \| __| |/ __/ __|
+//    | |  | (_| | |  |   <\__ \ | (_| |  __/ | | \ \ (_) | |_) | (_) | |_| | (__\__ \
+//    |_|   \__,_|_|  |_|\_\___/_|\__,_|\___| |_|  \_\___/|_.__/ \___/ \__|_|\___|___/
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
 #include "cammath.h"
 #include "draw.h"
 #include <kipr/camera.h>
@@ -26,7 +37,6 @@ bool isTrackable(int channel, int object)
     return false;
 }
 
-
 short findPixel(int b_color)
 {
     short color;
@@ -35,8 +45,8 @@ short findPixel(int b_color)
     char *image = get_camera_frame();
     short[][] pos;
     int x, y;
-    for(x=0;x<h;x++)
-        for(y=0;y<w;y++)
+    for(x=0;x<w;x++)
+        for(y=0;y<h;y++)
         {
             point2 p2;
             p2.x = x; p2.y = y;
@@ -45,8 +55,21 @@ short findPixel(int b_color)
                 case 1: color = p.r; break;
                 case 2: color = p.g; break;
                 case 3: color = p.b; break;
-                default color = -1;   
+                default: color = -1;   
+            }
+            if(color >= 127) {
+                short *ret;
+                ret = pos;
+                return ret;
             }
             pos[x][y] = &image + sizeof(image) + (h*x + y);
+
+            return image;
         }
+    return NULL;
+}
+
+char* edgeFilter(char *image)
+{
+    
 }
